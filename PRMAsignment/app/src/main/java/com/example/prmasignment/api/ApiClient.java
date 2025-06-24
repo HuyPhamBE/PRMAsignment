1fb6bf9d5357d6c97658a28a338f51167545a7a2
nqbao1002 <nqbao113@gmail.com>
2025-06-24 15:41:05 +0700

    add api client to connect to BE

---
+++ PRMAsignment/app/src/main/java/com/example/prmasignment/api/ApiClient.java
@@ -0,0 +1,18 @@
+package com.example.prmasignment.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://localhost:8080/")  // IP server Spring Boot
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
